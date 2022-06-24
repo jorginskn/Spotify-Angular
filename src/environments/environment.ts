dfaf63e1bc8cf3f95abd208ff73b08660992a6dd
Jorge Martins Ferraz <jorge.ferraz@b2ml.com.br>
2022-06-23 22:19:20 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,21 @@
 export const environment = {
   production: false
 };
+export const SpotifyConfiguration = {
+  clienteId: '5ab414ae82fc453b8bf85863c9f033fb',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'https://localhost:4200/login',
+  scopes: [
+    "user-read-currently-playing", // musica tocando agora.
+    "user-read-recently-played", // ler musicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuario
+    "user-top-read", // top artistas e musicas do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
+    "user-library-read", // ler biblioteca dos usuarios
+    "playlist-read-private", // ler playlists privads
+    "playlist-read-collaborative" // ler playlists colaborativas
+  ]
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
